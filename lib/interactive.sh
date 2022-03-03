c3f9043d0c92007f20ceedde9ffbcc9867c7bdc5
guvra <guvra@smile.fr>
2022-03-03 21:43:46 +0100

    Init script: better data validaiton / better configuration for smile packagist repository

--- lib/interactive.sh
+++ lib/interactive.sh
@@ -2,18 +2,6 @@
 
 # Do not run this script directly, it is a source file for the main setup script.
 
-validate_magento_edition () {
-    [[ "$1" = "community" || "$1" = "enterprise" || "$1" = "cloud" ]]
-}
-
-validate_magento_version () {
-    [[ " ${MAGENTO_VERSIONS[*]} " = *" $1 "* ]]
-}
-
-validate_answer () {
-    [[ "$1" = "y" || "$1" = "n" ]]
-}
-
 ask_input() {
     unset RESULT
     QUESTION=$2
@@ -75,9 +63,6 @@ if [ -z "${MAGENTO_EDITION}" ]; then
         ask_input MAGENTO_EDITION "Please choose the Magento edition to use (${BLUE}community${GREEN}, ${BLUE}enterprise${GREEN} or ${BLUE}cloud${GREEN})." ${DEFAULT_MAGENTO_EDITION}
         ! validate_magento_edition "${MAGENTO_EDITION}" && echo "${RED}The magento edition \"${MAGENTO_EDITION}\" is not valid.${RESET}"
     do true; done
-elif ! validate_magento_edition ${MAGENTO_EDITION}; then
-    echo -e "\n${RED}The magento edition \"${MAGENTO_EDITION}\" is not valid.${RESET}"
-    exit 1
 fi
 
 if [ "${MAGENTO_EDITION}" == "cloud" ]; then
@@ -85,19 +70,14 @@ if [ "${MAGENTO_EDITION}" == "cloud" ]; then
     echo -e "\n${RED}Warning:${RESET} Magento cloud projects must use the public packagist https://packagist.smile.fr, which requires an authenticated user account."
     echo "If you don't already have one, please ask dirtech@smile.fr to create an account for your project."
 
-    # Prevent script execution if magento git URL is missing and interactive mode is disabled
-    if [ "${NO_INTERACTION}" = "y" ]; then
-        if [ -z "${MAGENTO_GIT_URL}" ]; then
-            echo "${RED}Missing git repository URL.${RESET}"
-            exit 1
-        fi
+    if [ -z "${MAGENTO_GIT_URL}" ]; then
+        [ "${NO_INTERACTION}" = "y" ] && echo "${RED}Missing git repository URL.${RESET}" && exit 1
+        MAGENTO_GIT_URL=""
+        ask_required_input MAGENTO_GIT_URL "Please specify the .git url (git@...) of your Magento Cloud project."
     fi
 
-    MAGENTO_GIT_URL=""
-    ask_required_input MAGENTO_GIT_URL "Please specify the .git url (git@...) of your Magento Cloud project."
-
     MAGENTO_VERSION="${MAGENTO_VERSIONS[0]}"
-    DEFAULT_MAGENTO_REPOSITORY=${DEFAULT_MAGENTO_REPOSITORY_CLOUD}
+    SMILE_PACKAGIST=public
 else
     # Choice: Magento version
     if [ -z "${MAGENTO_VERSION}" ]; then
@@ -106,38 +86,35 @@ else
             ask_input MAGENTO_VERSION "Please choose the Magento version to use." ${DEFAULT_MAGENTO_VERSION}
             ! validate_magento_version "${MAGENTO_VERSION}" && echo "${RED}The magento version \"${MAGENTO_VERSION}\" is not valid.${RESET}"
         do true; done
-    elif ! validate_magento_version ${MAGENTO_VERSION}; then
-        echo -e "\n${RED}The magento version \"${MAGENTO_VERSION}\" is not valid.${RESET}"
-        exit 1
+    fi
+
+    # Choice: Magento package repository
+    if [ -z "${SMILE_PACKAGIST}" ]; then
+        SMILE_PACKAGIST=""
+        while
+            ask_input SMILE_PACKAGIST "Please choose the Smile packagist repository to use (${BLUE}internal${GREEN} or ${BLUE}public${GREEN})." ${DEFAULT_SMILE_PACKAGIST}
+            ! validate_smile_packagist "${SMILE_PACKAGIST}" && echo "${RED}The magento repository \"${SMILE_PACKAGIST}\" is not valid.${RESET}"
+        do true; done
     fi
 fi
 
-# Choice: Magento repository
-if [ -z "${MAGENTO_REPOSITORY}" ]; then
-    MAGENTO_REPOSITORY=""
-    ask_input MAGENTO_REPOSITORY "Please choose the Magento repository to use." ${DEFAULT_MAGENTO_REPOSITORY}
-fi
+# Set the composer repository URLs
+MAGENTO_PACKAGIST_URL="https://packagist.galaxy.intranet/mirror/magento_official/" && [ "${SMILE_PACKAGIST}" = "public" ] && MAGENTO_PACKAGIST_URL="https://repo.magento.com/"
+SMILE_PACKAGIST_URL="https://packagist.galaxy.intranet" && [ "${SMILE_PACKAGIST}" = "public" ] && SMILE_PACKAGIST_URL="https://packagist.smile.fr"
 
 # Ask for packagist ACLs if public smile packagist was selected (optional values)
-if [[ "${MAGENTO_REPOSITORY}" = "${DEFAULT_MAGENTO_REPOSITORY_CLOUD}" && "${NO_INTERACTION}" != "y" ]]; then
-    # Prevent script execution if packagist ACLs are missing and interactive mode is disabled
-    if [ "${NO_INTERACTION}" = "y" ]; then
-        if [ -z "${SMILE_PACKAGIST_USER}" ]; then
-            echo "${RED}Missing Smile packagist ACL user.${RESET}"
-            exit 1
-        fi
-
-        if [ -z "${SMILE_PACKAGIST_PASSWORD}" ]; then
-            echo "${RED}Missing Smile packagist ACL password.${RESET}"
-            exit 1
-        fi
+if [ "${SMILE_PACKAGIST}" = "public" ]; then
+    if [ -z "${SMILE_PACKAGIST_USER}" ]; then
+        [ "${NO_INTERACTION}" = "y" ] && echo "${RED}Missing Smile packagist ACL user.${RESET}" && exit 1
+        SMILE_PACKAGIST_USER=""
+        ask_required_input SMILE_PACKAGIST_USER "Please specify the Smile packagist Public ACL username to use."
     fi
 
-    SMILE_PACKAGIST_USER=""
-    ask_required_input SMILE_PACKAGIST_USER "Please specify the Smile packagist Public ACL username to use."
-
-    SMILE_PACKAGIST_PASSWORD=""
-    ask_required_input SMILE_PACKAGIST_PASSWORD "Please specify the Smile packagist Public ACL password to use."
+    if [ -z "${SMILE_PACKAGIST_PASSWORD}" ]; then
+        [ "${NO_INTERACTION}" = "y" ] && echo "${RED}Missing Smile packagist ACL password.${RESET}" && exit 1
+        SMILE_PACKAGIST_PASSWORD=""
+        ask_required_input SMILE_PACKAGIST_PASSWORD "Please specify the Smile packagist Public ACL password to use."
+    fi
 fi
 
 # Choice: Smile modules
@@ -155,11 +132,12 @@ fi
 # Summary
 echo -e "\n${GREEN}Summary${RESET}"
 echo "- Magento edition: ${BLUE}${MAGENTO_EDITION}${RESET}"
-echo "- Magento version: ${BLUE}${MAGENTO_VERSION}${RESET}"
-echo "- Magento repository: ${BLUE}${MAGENTO_REPOSITORY}${RESET}"
-if [ "${MAGENTO_REPOSITORY}" = "${DEFAULT_MAGENTO_REPOSITORY_CLOUD}" ]; then
-    echo "    - Packagist ACL user: ${BLUE}${SMILE_PACKAGIST_USER}${RESET}"
-    echo "    - Packagist ACL password: ${BLUE}${SMILE_PACKAGIST_PASSWORD}${RESET}"
+[ "${MAGENTO_EDITION}" = "cloud" ] && echo "- Magento git URL: ${BLUE}${MAGENTO_GIT_URL}${RESET}"
+[ "${MAGENTO_EDITION}" != "cloud" ] && echo "- Magento version: ${BLUE}${MAGENTO_VERSION}${RESET}"
+echo "- Packagist repository: ${BLUE}${SMILE_PACKAGIST_URL}${RESET}"
+if [ "${SMILE_PACKAGIST}" = "public" ]; then
+    echo "- Packagist ACL user: ${BLUE}${SMILE_PACKAGIST_USER}${RESET}"
+    echo "- Packagist ACL password: ${BLUE}${SMILE_PACKAGIST_PASSWORD}${RESET}"
 fi
 echo "- Smile modules: ${BLUE}${SMILE_MODULES}${RESET}"
 echo "- Smile tools: ${BLUE}${SMILE_TOOLS}${RESET}"
