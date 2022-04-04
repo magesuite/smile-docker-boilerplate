a785005976e1ce311e710863da8d0f1ca0a4d35e
guvra <guvra@smile.fr>
2022-04-04 10:02:36 +0200

    Remove PHP substitutions in docker entrypoint (not executable by app user)

--- images/php/7.4-cli/docker-entrypoint.sh
+++ images/php/7.4-cli/docker-entrypoint.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Update UID/GID
-if [ -n "${DOCKER_UID}" ]; then
-    usermod --uid $DOCKER_UID www-data
-fi
-
-if [ -n "${DOCKER_GID}" ]; then
-    groupmod --gid $DOCKER_GID www-data
-fi
-
-# Substitute php.ini values
-[ ! -z "${PHP_MEMORY_LIMIT}" ] && sed -i "s~!PHP_MEMORY_LIMIT!~${PHP_MEMORY_LIMIT}~" /usr/local/etc/php/conf.d/zz-magento.ini
-
-# Toggle Xdebug
-[ "$PHP_ENABLE_XDEBUG" = "true" ] && \
-    docker-php-ext-enable xdebug && \
-    echo "Xdebug is enabled"
-
 # Configure composer
 [ ! -z "${COMPOSER_GITHUB_TOKEN}" ] && \
     composer config --global github-oauth.github.com $COMPOSER_GITHUB_TOKEN
